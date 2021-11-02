31fd082e73e0dfe6464d4ebf6e4c7b1d2744fd4e
Brian Beckett <brian.beckett.esq@gmail.com>
2021-11-02 22:52:10 +0000

    Add MacOS support for The Key (Massdrop/Stack Overflow)
    Keymaps:
    - default-macos: cmd, c, v
    - url-copy-paste-macos: navigate to SO, copy, paste

---
+++ keyboards/massdrop/thekey/keymaps/url-copy-paste-macos/keymap.c
@@ -0,0 +1,56 @@
+/* Copyright 2021 Joe Maples <joe@maples.dev>, Brian Beckett <brian.beckett.esq@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+    TK_URL = SAFE_RANGE,
+    TK_COPY,
+    TK_PASTE,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        TK_URL,    TK_COPY,    TK_PASTE     
+    ),
+
+};
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TK_URL:
+            if (record->event.pressed) {
+                // when keycode TK_URL is pressed
+		// Open new tab, focus on address bar, navigate to Stack Overflow!
+                SEND_STRING(SS_LCMD("t") SS_LCMD("l") "https://stackoverflow.com/\n");
+            }
+            break;
+        case TK_COPY:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCMD("c"));
+            }
+            break;
+        case TK_PASTE:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCMD("v"));
+            }
+        default:
+            break;
+    }
+    return true;
+}
